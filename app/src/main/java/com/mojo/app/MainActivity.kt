8809eb70db4a8d7a2252ba3645e255047716dda9
Arlind <arlind.hajredinaj@souncloud.com>
2022-04-05 13:11:33 +0100

    add json input file. Read from json file from resourses

--- app/src/main/java/com/mojo/app/MainActivity.kt
+++ app/src/main/java/com/mojo/app/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mojo.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("ARLINDO", readRawJson())
+    }
+
+    private fun readRawJson(): String {
+        resources.openRawResource(R.raw.input).bufferedReader().use {
+            return it.readText()
+        }
     }
 }
