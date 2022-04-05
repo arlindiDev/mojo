edabbd5efe607f618425d5b5655c1f5abbade1a6
Arlind <arlind.hajredinaj@souncloud.com>
2022-04-05 23:06:20 +0100

    fix calculating padding

--- app/src/main/java/com/mojo/app/drawing/PaddingBounds.kt
+++ app/src/main/java/com/mojo/app/drawing/PaddingBounds.kt
@@ -3,14 +3,18 @@ package com.mojo.app.drawing
 import android.graphics.RectF
 
 fun RectF.withPadding(padding: Double): RectF {
+    if(padding == 0.0) {
+        return this
+    }
+
     val width = right - left
     val height = bottom - top
 
     return this.copy(
-        left = width - width * padding.invert(),
-        right = width * padding.invert(),
-        top = height - height * padding.invert(),
-        bottom = height * padding.invert(),
+        left = left + width - width * padding.invert(),
+        right = right - width * padding.toFloat(),
+        top = top + height - height * padding.invert(),
+        bottom = bottom - height * padding.toFloat(),
     )
 }
 
